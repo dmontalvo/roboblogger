eea157a199e5faf0da0cf8946df13a8592cdc390
Daniel Montalvo <daniel.m@archive.org>
2010-06-01 18:10:11 +0000

    First working version

--- roboblogger.py
+++ roboblogger.py
@@ -1,10 +1,15 @@
 import feedparser
+import pyblog
+from datetime import datetime
+from sys import argv
+script, username, password = argv
 old_posts = "posts.txt"
 
 def aggregateBlogs(feedList, aggBlog):
     """Takes a list of blog feeds and an aggregator blog. Adds all unaggregated posts from the blogs in the list and adds them to the aggregator blog."""
     parsedList = parseFeeds(feedList)
     newPostList = getNewPosts(parsedList)
+    postToWordPress(newPostList, aggBlog)
 
 def parseFeeds(feedList):
     """Takes a list of feeds and returns them in parsed form."""
@@ -29,9 +34,26 @@ def isPosted(post):
     f.close()
     return urls.find(post.link) != -1
 
-def postToWordpress(postList, aggBlog):
+def postToWordPress(postList, aggBlog):
     """Takes a list of posts and the aggBlog and then adds the posts to the aggBlog. Writes the url of the post to a file (to record it as "old")."""
+    blog = pyblog.WordPress(aggBlog, username, password)
+    for post in postList:
+        content = post.content[0].value
+        title = post.title
+        date = datetime(*post.updated_parsed[:6])
+        postDict = {'title':title, 'description':content, 'dateCreated':date}
+        blog.new_post(postDict)
+    updateFile(postList)
 
-blogs = ["http://blog.openlibrary.org/feed/", "http://internetarchive.wordpress.com/feed/", "http://www.opencontentalliance.org/feed/"]
+def updateFile(postList):
+    """Writes the posts in the list to a file."""
+    f = open(old_posts, 'a')
+    for post in postList:
+        f.write(post.link)
+        f.write("\n")
+    f.close()
 
-aggregateBlogs(blogs, "http://www.opencontentalliance.org/feed/")
+# blogs = ["http://blog.openlibrary.org/feed/", "http://internetarchive.wordpress.com/feed/", "http://www.opencontentalliance.org/feed/"]
+
+blogs = ["http://blog.openlibrary.org/feed/"]
+aggregateBlogs(blogs, "http://dmontalvo.wordpress.com/xmlrpc.php")
